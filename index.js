6e549604579d4b210cd7ee61d18d4b7e46eaf7a6
INBOOK_X1_PRO\asdvb <nathachai13011997@gmail.com>
2022-01-05 08:33:16 +0700

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -20,7 +21,9 @@ const likesRouter = require("./routes/Likes");
 app.use("/likes", likesRouter);
 
 db.sequelize.sync().then(()=>{
-    app.listen(3001, ()=>{
+    app.listen(process.env.PORT || 3001, ()=>{
         console.log('Server runing on port 3001');
     });
-});
+}).catch((err)=>{
+        console.log(err);
+})
